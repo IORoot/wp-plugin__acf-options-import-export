a2cc75a69e625018bcd7455df298b9748bcaac3f
Andy Pearson <andy@londonparkour.com>
2020-05-25 16:14:16 +0100

    Add emoji description

--- import_export.php
+++ import_export.php
@@ -7,7 +7,7 @@
  * @wordpress-plugin
  * Plugin Name:       _ANDYP - ACF Import / export for options pages
  * Plugin URI:        http://londonparkour.com
- * Description:       <strong">TOOL</strong> | <em> Custom Fields > ACF import/export.</em> | Allows you to import and export all option DATA from the DB. 
+ * Description:       <strong">🔧TOOL</strong> | <em> Custom Fields > ACF import/export.</em> | Allows you to import and export all option DATA from the DB. 
  * Version:           1.0.0
  * Author:            Andy Pearson
  * Author URI:        https://londonparkour.com
