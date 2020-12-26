fe5040b4a550e317eb065a9b89fffb7486dc0a29
Andy Pearson <andy@londonparkour.com>
2020-12-26 13:34:08 +0000

    Reorganise

--- src/acf/andyp_plugin_register.php
+++ src/acf/andyp_plugin_register.php
@@ -4,7 +4,7 @@ add_action( 'plugins_loaded', function() {
     do_action('register_andyp_plugin', [
         'title'     => 'ACF Data Import/Export',
         'icon'      => 'database-export',
-        'color'     => '#FF5252',
+        'color'     => '#242424',
         'path'      => __FILE__,
     ]);
 } );
