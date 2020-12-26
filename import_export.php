20e2e38a2a66e1aac5d6fd21855f66dc49e67cdc
Andy Pearson <andy@londonparkour.com>
2020-12-26 10:56:15 +0000

    rename

--- import_export.php
+++ import_export.php
@@ -1,11 +1,7 @@
 <?php
 /*
- * @package   ACF options importer exporter
- * @author    Andy Pearson <andy@londonparkour.com>
- * @copyright 2020 LondonParkour
- *
  * @wordpress-plugin
- * Plugin Name:       _ANDYP - ACF Import / export for options pages
+ * Plugin Name:       _ANDYP - ACF - Import / Export options data
  * Plugin URI:        http://londonparkour.com
  * Description:       <strong">🔧TOOL</strong> | <em> Custom Fields > ACF import/export.</em> | Allows you to import and export all option DATA from the DB. 
  * Version:           1.0.0
